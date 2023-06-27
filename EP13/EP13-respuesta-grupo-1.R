363c5ed54623185ba8a3860f8b7ec8a5cbda4bba
Angel Vilches Urrutia <angel.vilches@usach.cl>
2023-06-27 15:13:24 -0400

    Correcciones

--- EP13/EP13-respuesta-grupo-1.R
+++ EP13/EP13-respuesta-grupo-1.R
@@ -25,6 +25,7 @@ datos <- read.csv2(file.choose(),
 ################################################################################
 ############################# Pregunta 1 - Grupo 1 #############################
 ################################################################################
+cat("######################### Pregunta 1 - Grupo 1 ########################\n")
 
 # Definir la semilla a utilizar, que corresponde a los últimos cuatro dígitos
 # del RUN (sin considerar el dígito verificador) del integrante de menor edad
@@ -35,10 +36,12 @@ datos <- read.csv2(file.choose(),
 # Los últimos cuatro dígitos del RUN son               : 6 0 6 4
 # A continuación se define la semilla según lo indicado:
 set.seed(6064)
+cat("Semilla establecida: 6 0 6 4\n")
 
 ################################################################################
 ############################# Pregunta 2 - Grupo 1 #############################
 ################################################################################
+cat("######################### Pregunta 2 - Grupo 1 ########################\n")
 
 # Seleccionar una muestra de 100 vinos.
 
@@ -46,10 +49,12 @@ set.seed(6064)
 # obtenidos (EP13 Datos.csv)
 
 muestra <- sample_n(datos, 100)
+cat("Extracción de muestra de 100 vinos de acuerdo con los datos obtenidos\n")
 
 ################################################################################
 ############################# Pregunta 3 - Grupo 1 #############################
 ################################################################################
+cat("######################### Pregunta 3 - Grupo 1 ########################\n")
 
 # Seleccionar de forma aleatoria 6 posibles variables predictoras.
 
@@ -108,50 +113,76 @@ print(variables_predictoras)
 ################################################################################
 ############################# Pregunta 4 - Grupo 1 #############################
 ################################################################################
+cat("######################### Pregunta 4 - Grupo 1 ########################\n")
 
 # Seleccionar, entre las variables que no fueron escogidas en el punto anterior,
 # una que el equipo considere que podría ser útil para predecir la variable
 # calidad, justificando bien esta selección.
 
-# Se opta por seleccionar la variable "pH".
-# En este sentido, el pH del vino puede influir en su sabor, equilibrio y
-# estabilidad, lo cual puede afectar la percepción de su calidad. Además, 
-# el pH puede interactuar con otras características químicas presentes en el
-# vino, como la acidez y los sulfatos. Por lo tanto, consideramos que la 
-# variable "pH" es relevante para la predicción de la calidad.
+# Se opta por seleccionar la variable "alcohol".
+# En este sentido, el alcohol puede influir en varios aspectos de la experiencia
+# de degustación, como el sabor, el cuerpo y la sensación. Por lo tanto, 
+# seleccionar el contenido de alcohol como una variable adicional para predecir
+# la calidad del vino es razonable y relevante para la predicción de la calidad.
 
-variable_seleccionada <- names(muestra)[11]
+variable_seleccionada <- names(muestra)[13]
 cat("Variable predictora seleccionada: \n")
 print(variable_seleccionada)
 
 ################################################################################
 ############################# Pregunta 5 - Grupo 1 #############################
 ################################################################################
+cat("######################### Pregunta 5 - Grupo 1 ########################\n")
 
 # Usando el entorno R, construir un modelo de regresión lineal simple con el
 # predictor seleccionado en el paso anterior.
 
-modelo_RLS <- lm(calidad ~ ph,
+# Previo a la realización del modelo, se calcula la correlación entre la 
+# variable calidad y el predictor seleccionado, es decir, pH
+
+R <- cor(muestra[["alcohol"]], muestra[["calidad"]])
+cat("Correlación: ", R)
+
+# El valor de correlación anteriormente obtenido, este es, 0.418955. Sugiere una
+# correlación positiva moderada entre el contenido de alcohol y la calidad del
+# vino.
+
+modelo_RLS <- lm(calidad ~ alcohol,
                  data = muestra)
 print(summary(modelo_RLS))
 
 # Se grafica el modelo
 grafico_modelo_RLS <- ggscatter(datos,
-                                x = "ph",
+                                x = "alcohol",
                                 y = "calidad",
                                 color = "purple",
                                 fill = "purple",
-                                xlab = "pH",
+                                xlab = "Alcohol",
                                 ylab = "Calidad",
                                 xticks.by = 1)
 grafico_modelo_RLS <- grafico_modelo_RLS + geom_smooth(method = lm,
                                                        se = FALSE,
-                                                       colour = "purple")
+                                                       colour = "red")
 print(grafico_modelo_RLS)
+
+# Según lo graficado, se observa una que la línea roja indica la tendencia
+# estimada de la relación entre la variable "alcohol" en el eje x y la variable
+# "calidad" en el eje y. Dado que la línea roja se extiende desde el punto 5
+# hasta el punto 7 en el eje y, se sugiere que a medida que aumenta el contenido
+# de alcohol, se espera un aumento en la calidad del vino.
+# La concentración de observaciones en los valores centrales del eje y (5, 6 y
+# 7) indica que hay una mayor cantidad de vinos con calidades en ese rango
+# específico en la muestra. Es decir, se indica que los vinos de calidad
+# promedio (representados por los valores centrales) son más comunes en la
+# muestra que los vinos de baja o alta calidad.
+# La dispersión de puntos alrededor de la línea de regresión indica que, si bien
+# existe una tendencia general de aumento en la calidad del vino con el
+# contenido de alcohol, todavía hay cierta variabilidad en los datos. 
 
 ################################################################################
 ############################# Pregunta 6 - Grupo 1 #############################
 ################################################################################
+cat("######################### Pregunta 6 - Grupo 1 ########################\n")
 
 # Agregue la variable seleccionada en el paso 4 al conjunto obtenido en el
 # punto 3.
@@ -165,31 +196,40 @@ print(variables_predictoras)
 ################################################################################
 ############################# Pregunta 7 - Grupo 1 #############################
 ################################################################################
+cat("######################### Pregunta 7 - Grupo 1 ########################\n")
 
 # Usando herramientas para la exploración de modelos del entorno R, escoger 
 # entre dos y cinco predictores de entre las variables presentes en el conjunto
 # obtenido en el paso anterior para construir un modelo de regresión lineal
 # múltiple.
 
-# Se confecciona un dataframe con las variables predictoras seleccionadas
-datos_modelo <- muestra[, c("calidad", variables_predictoras)]
-
-# Crear el modelo de regresión lineal múltiple utilizando stepwise
-modelo_multiple <- lm(calidad ~ ., data = datos_modelo)
-modelo_final <- step(modelo_multiple)
-
-# Mostrar un resumen del modelo final
-summary(modelo_final)
+modelo_RLM <- lm(calidad ~ .,
+                 data = muestra[, c("calidad", variables_predictoras)])
+print(summary(modelo_RLM))
+grafico_modelo_RLM <- ggplot(data = muestra,
+                             aes(x = calidad,
+                                 y = predict(modelo_RLM))) +
+                      geom_point(color = "purple",
+                                 fill = "purple") +
+                      geom_smooth(method = "lm",
+                                  se = FALSE,
+                                  color = "red") +
+                      xlab("Calidad") +
+                      ylab("Predicción") +
+                      ggtitle("Modelo de Regresión Lineal Múltiple")
+print(grafico_modelo_RLM)
 
 ################################################################################
 ############################# Pregunta 8 - Grupo 1 #############################
 ################################################################################
+cat("######################### Pregunta 8 - Grupo 1 ########################\n")
 
 
 
 ################################################################################
 ############################# Pregunta 9 - Grupo 1 #############################
 ################################################################################
+cat("######################### Pregunta 9 - Grupo 1 ########################\n")
 
 
 
