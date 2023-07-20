a73bad01e016950acd28233aa0467dd9081c6538
Angel Vilches Urrutia <angel.vilches@usach.cl>
2023-07-20 19:37:41 -0400

    EP15 punto 1 y 2

---
+++ EP15/EP15-respuesta-grupo-1.R
@@ -0,0 +1,103 @@
+# Integrantes - Grupo 1
+# Jaime Carrasco Quintrequeo
+# Andrés Haussmann Garín
+# Benjamín González Hurtado
+# Ángel Vilches Urrutia
+
+# Librerías
+
+if (!require(caret)) {
+  install.packages("caret",
+                   dependencies = TRUE)
+  require(caret)
+}
+
+if (!require(leaps)) {
+  install.packages("leaps",
+                   dependencies = TRUE)
+  require(leaps)
+}
+
+# Primeramente, se obtienen los datos de la actividad
+
+datos <- read.csv2(file.choose(),
+                   stringsAsFactors = TRUE)
+
+################################################################################
+############################# Pregunta 1 - Grupo 1 #############################
+################################################################################
+cat("######################### Pregunta 1 - Grupo 1 ########################\n")
+
+# Definir la semilla a utilizar, que corresponde a los primeros cinco dígitos
+# del RUN del integrante de mayor edad del equipo
+
+# El integrante de mayor edad es                       : Andrés Haussmann G. 
+# El rut del integrante de menor edad es               : 18.972.186-2
+# Los últimos cuatro dígitos del RUN son               : 1 8 9 7 2
+# A continuación se define la semilla según lo indicado:
+set.seed(18972)
+cat("Semilla establecida: 1 8 9 7 2\n")
+
+################################################################################
+############################# Pregunta 2 - Grupo 1 #############################
+################################################################################
+cat("######################### Pregunta 2 - Grupo 1 ########################\n")
+
+# Seleccionar una muestra de 100 vinos, asegurando que la mitad sean blancos y
+# la otra mitad, tintos.
+
+vinos_blancos <- subset(datos,
+                        clase == "Blanco")
+vinos_tintos <- subset(datos,
+                       clase == "Tinto")
+muestra_blancos <- vinos_blancos[sample(nrow(vinos_blancos),
+                                        50), ]
+muestra_tintos <- vinos_tintos[sample(nrow(vinos_tintos),
+                                      50), ]
+
+# En este punto, se dispone de dos muestras cuya calidad refiere a blanco y 
+# tinto. Ambas muestras de tamaño: 50. Se procede a unificar las muestras
+
+muestra_final <- rbind(muestra_blancos,
+                       muestra_tintos)
+
+################################################################################
+############################# Pregunta 3 - Grupo 1 #############################
+################################################################################
+cat("######################### Pregunta 3 - Grupo 1 ########################\n")
+
+# Usando las herramientas del paquete leaps, realizar una búsqueda exhaustiva 
+# para seleccionar entre 2 y 6 predictores que ayuden a estimar la variable 
+# calidad y luego utilizar las funciones del paquete caret para construir un
+# modelo de regresión lineal múltiple con los predictores escogidos y evaluarlo
+# usando bootstrapping.
+
+res <- regsubsets(calidad ~ .,
+           data   = datos,
+           nbest  = 6,
+           nvmax  = 6,
+           method = "exhaustive")
+plot(res)
+
+# Seleccionar el mejor conjunto de predictores basado en el rendimiento
+mjrs_predictores <- names(coef(res,
+                               which.max(summary(res)$adjr2)))
+
+# Omitir la variable "Intercept" del conjunto de predictores
+mjrs_predictores <- mjrs_predictores[mjrs_predictores != "(Intercept)"]
+
+# Construir el modelo de regresión lineal múltiple utilizando caret
+modelo_RLM <- lm(calidad ~ .,
+                 data = muestra_final[, c("calidad",
+                                          mjrs_predictores)])
+
+# Evaluar el modelo utilizando bootstrapping con caret
+boot_control <- trainControl(method = "boot",
+                             number = 100)
+modelo_RLM_boot <- train(calidad ~ .,
+                         data = muestra_final[, c("calidad",
+                                                  mjrs_predictores)],
+                         method = "lm", trControl = boot_control)
+
+# Ver el resumen del rendimiento del modelo con bootstrapping
+print(modelo_RLM_boot)
